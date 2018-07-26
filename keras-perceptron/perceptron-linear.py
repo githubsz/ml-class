4a248bbbdbc5203bac1ca8411daf75f54a01148a
shaoyan33 <shaoyan33@local>
2018-07-26 23:29:30 +0000

    my changes

--- keras-perceptron/perceptron-linear.py
+++ keras-perceptron/perceptron-linear.py
@@ -16,6 +16,12 @@ config = run.config
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
 
+# normalize data
+X_train = X_train.astype('float32')
+X_train /= 255.
+X_test = X_test.astype('float32')
+X_test /= 255.
+
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -26,9 +32,25 @@ num_classes = y_train.shape[1]
 # create model
 model=Sequential()
 model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(num_classes))
-model.compile(loss='mse', optimizer='adam',
+# model.add(Dense(num_classes))
+model.add(Dropout(0.5))
+model.add(Dense(100, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation="softmax"))
+#model.add(Dropout(0.5))
+#model.add(Dense(100, activation="relu"))
+#model.add(Dropout(0.5))
+#model.add(Dense(num_classes, activation="softmax"))
+#model.add(Dropout(0.5))
+# model.add(Dense(100, activation="softmax"))
+# model.add(Dense(100, activation="softmax"))
+model.add(Dense(num_classes, activation="softmax"))
+#model.add(Dropout(0.5))
+#model.compile(loss='mse', optimizer='adam',
+#                metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='Adamax',
                 metrics=['accuracy'])
+
 
 # Fit the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
--- keras-perceptron/perceptron-single.py
+++ keras-perceptron/perceptron-single.py
@@ -23,7 +23,8 @@ img_height = X_train.shape[2]
 # create model
 model=Sequential()
 model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(1, activation="sigmoid"))
+# model.add(Dense(1, activation="sigmoid"))
+model.add(Dense(1, activation="softmax"))
 model.compile(loss='binary_crossentropy', optimizer='adam',
                 metrics=['accuracy'])
 
